fb42ac4aa52a1d2ac2985c0d5fd595e0665776db
Mariana Pantelic <mari27line@yahoo.com>
2021-08-10 15:55:17 +0200

    fixed navbar errors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -68,72 +68,60 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
+      </div>
+      <div class="dropdown user-dropdown">
+        <div>
+          {localStorage.getItem("token") ? (
+            <div href="/profile" className="d-flex username">
+              <span className="mx-2 text-white mt-3">
+                {user && user.userName}
+              </span>
+            </div>
+          ) : null}
+        </div>
+        <button
+          class="btn btn-transparent dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="user"
+            class="svg-inline--fa fa-user fa-w-14 userLogin"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              fill="currentColor"
+              d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
+            ></path>
+          </svg>
+        </button>
 
-        <div className="d-flex">
-          <div>
+        <div class="dropdown-menu register-buttons">
+          <a class="dropdown-item" href="/login">
+            {!localStorage.getItem("token") ? <div>Sign in</div> : null}
+          </a>
+          <a class="dropdown-item" href="/register">
+            {!localStorage.getItem("token") ? <div>Register</div> : null}
+          </a>
+          <a class="dropdown-item" href="/">
             {localStorage.getItem("token") ? (
-              <div href="/profile" className="d-flex">
-                <span className="mx-2 text-white mt-3">
-                  {user && user.userName}
-                </span>
+              <div onClick={logOut} className="logout">
+                Logout
               </div>
             ) : null}
-          </div>
-          <div class="dropdown userdropdown">
-            <button
-              class="btn btn-transparent dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="user"
-                class="svg-inline--fa fa-user fa-w-14 userLogin"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
-                ></path>
-              </svg>
-            </button>
-
-            <div class="dropdown-menu register-buttons">
-              <a class="dropdown-item" href="/login">
-                {!localStorage.getItem("token") ? (
-                  <div onClick={logOut} className="logout">
-                    Sign in
-                  </div>
-                ) : null}
-              </a>
-              <a class="dropdown-item" href="/register">
-                {!localStorage.getItem("token") ? (
-                  <div onClick={logOut} className="logout">
-                    Register
-                  </div>
-                ) : null}
-              </a>
-              <a class="dropdown-item" href="/">
-                {localStorage.getItem("token") ? (
-                  <div onClick={logOut} className="logout">
-                    Logout
-                  </div>
-                ) : null}
-              </a>
-            </div>
-          </div>
-
+          </a>
         </div>
       </div>
     </nav>
   );
- };
+};
 
 export default Navbar;
