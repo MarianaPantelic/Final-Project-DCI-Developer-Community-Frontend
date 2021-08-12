3120d99f300104b636fe34670c4aebc1b7cfeb4b
Heriberto <hrhproducciones@gmail.com>
2021-08-12 10:30:47 +0200

    Added Jobs pages * Modified main.css for jobs * Added Linus Torvalds pic * Added Jobs title background

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -3,9 +3,47 @@ import { Container } from "react-bootstrap";
 
 const Jobs = () => {
   return (
-    <Container>
-      <div></div>
-    </Container>
+    //<Container>
+      <div className="container2">
+        
+        <div className="Jobs-title">
+          <img src="images/jobs-title-background-desktop.png"/>
+          <div className="Jobs-text">Jobs</div>
+        </div>
+
+
+      <div className="content-jobs-page">
+        <div className="Job-list">
+          <p>Zapp (On-Demand Technology) - <a href="https://drive.google.com/file/d/14AmwI6DhZddwfTrk6XYXdgV7EMAQ85xN/view" target="_blank">Fullstack TypeScript Support Engineer</a></p>
+          <p>Global Savings Group (Commerce Content Platform) - <a href="https://global-savings-group.com/careers/?gh_jid=3208245" target="_blank">Frontend Engineer (m/f/d)</a></p>
+          <p>Climate Partner (Climate Protection) - <a href="https://goodjobs.eu/de/jobs/software-engineer-gn-climate-action-a91f45ec-f56f-4ab3-931f-1dd576c9f99d" target="_blank">Software Engineer - Climate Action</a></p>
+          <p>JOIN Solutions (Software Solutions Provider) - <a href="https://join.join.com/jobs/2676528-junior-frontend-developer-react-js-f-m-d" target="_blank">Junior Frontend Developer (React.js) (f/m/d)</a></p>
+          <p>Jimdo (IT Services) - <a href="https://jimdo.breezy.hr/p/a743116f15ed01-frontend-software-engineer-m-f-d" target="_blank">Frontend Software Engineer (m/f/d)</a></p>
+          <p>Reuter (E-Commerce) - <a href="https://jobs.reuter.de/job/657" target="_blank">Frontend Developer (m/f/d)</a></p>
+          <p>Truckoo (Purchasing Network for Truckers) - <a href="https://www.truckoo.com/careers" target="_blank">Frontend Engineer (f/m/d)</a></p>
+          <p>Pult (Digitalisation) - <a href="https://pult.join.com/jobs/2638772-full-stack-developer?widgetv2=true&pid=d73d1a20e99ab4ced633" target="_blank">Full-Stack Developer</a></p>
+          <p>hubblr (Software Development) - <a href="https://hubblr.join.com/jobs/2651937-software-engineer-web-frontend-f-m-x-hh?pid=93dde148e3f919be3310&utm_source=linkedin&utm_medium=free&utm_campaign=linkedin%2Blimited%2Blisting&utm_content=software%2Bengineer%2Bweb-frontend%2Bf%2Bm%2Bx%2Bhh" target="_blank">Software Enginner (Web-Frontend) (f/m/x)</a></p>
+          <p>Zapp (On-Demand Technology) - <a href="https://www.db-n.com/en/developer-frontend-f-m-d/" target="_blank">Developer Frontend (f/m/d)</a></p>
+          <p>Global Savings Group (Commerce Content Platform) - <a href="https://global-savings-group.com/careers/?gh_jid=3208245" target="_blank">Frontend Engineer (m/f/d)</a></p>
+          <p>Climate Partner (Climate Protection) - <a href="https://goodjobs.eu/de/jobs/software-engineer-gn-climate-action-a91f45ec-f56f-4ab3-931f-1dd576c9f99d" target="_blank">Software Engineer - Climate Action</a></p>
+          <p>JOIN Solutions (Software Solutions Provider) - <a href="https://join.join.com/jobs/2676528-junior-frontend-developer-react-js-f-m-d" target="_blank">Junior Frontend Developer (React.js) (f/m/d)</a></p>
+          <p>Jimdo (IT Services) - <a href="https://jimdo.breezy.hr/p/a743116f15ed01-frontend-software-engineer-m-f-d" target="_blank">Frontend Software Engineer (m/f/d)</a></p>
+          <p>Reuter (E-Commerce) - <a href="https://jobs.reuter.de/job/657" target="_blank">Frontend Developer (m/f/d)</a></p>
+          <p>Truckoo (Purchasing Network for Truckers) - <a href="https://www.truckoo.com/careers" target="_blank">Frontend Engineer (f/m/d)</a></p>
+          <p>Pult (Digitalisation) - <a href="https://pult.join.com/jobs/2638772-full-stack-developer?widgetv2=true&pid=d73d1a20e99ab4ced633" target="_blank">Full-Stack Developer</a></p>
+          <p>hubblr (Software Development) - <a href="https://hubblr.join.com/jobs/2651937-software-engineer-web-frontend-f-m-x-hh?pid=93dde148e3f919be3310&utm_source=linkedin&utm_medium=free&utm_campaign=linkedin%2Blimited%2Blisting&utm_content=software%2Bengineer%2Bweb-frontend%2Bf%2Bm%2Bx%2Bhh" target="_blank">Software Enginner (Web-Frontend) (f/m/x)</a></p>
+          <p>Global Savings Group (Commerce Content Platform) - <a href="https://global-savings-group.com/careers/?gh_jid=3208245" target="_blank">Frontend Engineer (m/f/d)</a></p>
+          <p>Climate Partner (Climate Protection) - <a href="https://goodjobs.eu/de/jobs/software-engineer-gn-climate-action-a91f45ec-f56f-4ab3-931f-1dd576c9f99d" target="_blank">Software Engineer - Climate Action</a></p>
+          <p>JOIN Solutions (Software Solutions Provider) - <a href="https://join.join.com/jobs/2676528-junior-frontend-developer-react-js-f-m-d" target="_blank">Junior Frontend Developer (React.js) (f/m/d)</a></p>
+          <p>Jimdo (IT Services) - <a href="https://jimdo.breezy.hr/p/a743116f15ed01-frontend-software-engineer-m-f-d" target="_blank">Frontend Software Engineer (m/f/d)</a></p>
+          <p>Reuter (E-Commerce) - <a href="https://jobs.reuter.de/job/657" target="_blank">Frontend Developer (m/f/d)</a></p>
+          <p>Truckoo (Purchasing Network for Truckers) - <a href="https://www.truckoo.com/careers" target="_blank">Frontend Engineer (f/m/d)</a></p>
+          <p>Pult (Digitalisation) - <a href="https://pult.join.com/jobs/2638772-full-stack-developer?widgetv2=true&pid=d73d1a20e99ab4ced633" target="_blank">Full-Stack Developer</a></p>
+          <p>hubblr (Software Development) - <a href="https://hubblr.join.com/jobs/2651937-software-engineer-web-frontend-f-m-x-hh?pid=93dde148e3f919be3310&utm_source=linkedin&utm_medium=free&utm_campaign=linkedin%2Blimited%2Blisting&utm_content=software%2Bengineer%2Bweb-frontend%2Bf%2Bm%2Bx%2Bhh" target="_blank">Software Enginner (Web-Frontend) (f/m/x)</a></p>
+        </div>
+        </div>
+      </div>
+    //</Container>
   );
 };
 
