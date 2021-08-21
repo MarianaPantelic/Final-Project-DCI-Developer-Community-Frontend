f6f6406b1b78b0cf4ecbc56c29f380aad2014b16
KinjallDodhiaa <kinjal_dodhia@yahoo.com>
2021-08-21 13:29:03 +0200

    changed the topic style. Added questions and answers from different user. Added a clip path to forum page

--- src/pages/forum.js
+++ src/pages/forum.js
@@ -10,47 +10,53 @@ const Forum = (props) => {
   console.log(props.show);
 
   return (
-    <Container className="mt-5">
-      <div className="text-center pt-5 pb-3">
-        {localStorage.getItem("token") ? (
-          <Link to="/addQuestions">
-            <button type="button" className="askQuestion-btn">
-              Ask Question
-            </button>
-          </Link>
-        ) : (
-          <Link to="/login">
-            <button type="button" className="askQuestion-btn">
-              Ask Question
-            </button>
-          </Link>
-        )}
+    <>
+      <div>
+        <div className="first-clip-forum"></div>
+        <div className="second-clip-forum"></div>
       </div>
-      <div className="container question-cards-container">
-        {props.show.map((question, index) => (
-          <div key={index} class="card mt-5 question-cards">
-            <div className="card-header pl-5 question-user">
-              {question.user ? question.user.firstName : ""}
+
+      <Container className="mt-5">  
+        <div className="text-center pt-5 pb-3">
+          {localStorage.getItem("token") ? (
+            <Link to="/addQuestions">
+              <button type="button" className="askQuestion-btn">
+                Ask Question
+              </button>
+            </Link>
+          ) : (
+            <Link to="/login">
+              <button type="button" className="askQuestion-btn">
+                Ask Question
+              </button>
+            </Link>
+          )}
+        </div>
+        <div className="container question-cards-container">
+          {props.show.map((question, index) => (
+            <div key={index} class="card mt-5 question-cards">
+              <div className="card-header pl-5 question-user">
+                {question.user ? question.user.firstName : ""}
+              </div>
+              <div className="card-body">
+                <blockquote class="blockquote mb-0 question-title">
+                  <Link
+                    to={`/showQuestion/${question._id}`}
+                    className="question-link"
+                  >
+                    <p className="p-4">{question.title}</p>
+                  </Link>
+                  <footer className="p-4">
+                    <div className="float-left topic">
+                      <p className="text-center">{question.topic}</p>
+                    </div>
+                  </footer>
+                </blockquote>
+              </div>
             </div>
-            <div className="card-body">
-              <blockquote class="blockquote mb-0 question-title">
-                <Link
-                  to={`/showQuestion/${question._id}`}
-                  className="question-link"
-                >
-                  <p className="p-4">{question.title}</p>
-                </Link>
-                <footer className="p-4">
-                  <div className="question-topic">
-                    <p className="text-center">{question.topic}</p>
-                  </div>
-                </footer>
-              </blockquote>
-            </div>
-          </div>
-        ))}
-      </div>
-      {/* <div className="blog-container d-flex flex-wrap justify-content-center ">
+          ))}
+        </div>
+        {/* <div className="blog-container d-flex flex-wrap justify-content-center ">
         {props.questions ? (
           props.questions.reverse().map((element) => {
             const converter = new QuillDeltaToHtmlConverter(
@@ -79,7 +85,8 @@ const Forum = (props) => {
           <div className="loader">LOADING...</div>
         )}
       </div> */}
-    </Container>
+      </Container>
+    </>
   );
 };
 
