cac185dff800b33d0488677d31691a074849f9e6
Mariana Pantelic <mari27line@yahoo.com>
2021-08-29 18:38:40 +0200

    add clips & fix responsivness myquestion, myblogs

--- src/pages/myQuestion.js
+++ src/pages/myQuestion.js
@@ -39,59 +39,62 @@ const MyQuestion = (props) => {
     window.location.replace("/userprofile");
   };
   return (
-    <Container>
-      <Col sm={12} md={12} lg={8} className="question-container">
-        {question ? (
-          <div>
-            <div className=" float-left topic">{question.topic}</div>
-            <div className="question-section">
-              <h1 className=" text-center">{question.title}</h1>
+    <>
+      <div className="my-clip2"></div>
+      <Container>
+        <Col sm={12} md={12} lg={8} className="question-container">
+          {question ? (
+            <div>
+              <div className=" float-left topic">{question.topic}</div>
+              <div className="question-section">
+                <h1 className=" text-center">{question.title}</h1>
 
-              <div
-                className="mt-5 text-center"
-                dangerouslySetInnerHTML={{
-                  __html: question.content,
-                }}
-              ></div>
-            </div>
-            <div className="float-right mt-2">
-              {question && question.answer && question.answer.length !== 0
-                ? question.answer.length
-                : 0}{" "}
-              answers
-            </div>
+                <div
+                  className="mt-5 text-center"
+                  dangerouslySetInnerHTML={{
+                    __html: question.content,
+                  }}
+                ></div>
+              </div>
+              <div className="float-right mt-2">
+                {question && question.answer && question.answer.length !== 0
+                  ? question.answer.length
+                  : 0}{" "}
+                answers
+              </div>
 
-            <div className="answers-section">
-              <ul>
-                {question.answer && question.answer.length !== 0
-                  ? question.answer.map((element) => (
-                      <div>
-                        <h2>Answers</h2>
-                        <div
-                          className="mt-5 text-center"
-                          dangerouslySetInnerHTML={{
-                            __html: element.content,
-                          }}
-                        ></div>
-                      </div>
-                    ))
-                  : null}
-              </ul>
+              <div className="answers-section">
+                <ul>
+                  {question.answer && question.answer.length !== 0
+                    ? question.answer.map((element) => (
+                        <div>
+                          <h2>Answers</h2>
+                          <div
+                            className="mt-5 text-center"
+                            dangerouslySetInnerHTML={{
+                              __html: element.content,
+                            }}
+                          ></div>
+                        </div>
+                      ))
+                    : null}
+                </ul>
+              </div>
             </div>
+          ) : null}
+
+          <div className="d-flex justify-content-around justify-content-center btn-container">
+            <Link to={`/updateMyQuestion/${id}`}>
+              <Button className="register-btn">Update</Button>
+            </Link>
+
+            <Button className="register-btn" onClick={deleteQuestion}>
+              Delete
+            </Button>
           </div>
-        ) : null}
-
-        <div className="d-flex justify-content-around justify-content-center btn-container">
-          <Link to={`/updateMyQuestion/${id}`}>
-            <Button className="register-btn">Update</Button>
-          </Link>
-
-          <Button className="register-btn" onClick={deleteQuestion}>
-            Delete
-          </Button>
-        </div>
-      </Col>
-    </Container>
+        </Col>
+      </Container>
+    </>
   );
 };
 
